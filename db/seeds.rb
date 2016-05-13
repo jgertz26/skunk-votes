de0d58bab7503bef4c200070512bb2d97a21cfc1
Jeremy <jgertz26@gmail.com>
2016-05-13 15:46:32 -0400

    add seeder file and fix a couple links

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+user = User.create(email:'dingding@paytronix.com', password: 'bingbong')
+project1 = Project.create(name: "Mobile Survey Taker",
+description: "Integrates with Paytronix server to allow users to take restaurant surveys on their phone",
+status: "completed")
+
+ProjectOwner.create(user: user, project: project1)
+
+project2 = Project.create(name: "Chicken Sandwich Machine",
+description: "This sandwich makes machines",
+status: "brainstorming")
+
+ProjectOwner.create(user: user, project: project2)
+
+project3 = Project.create(name: "Cure for World Hunger",
+description: "This is also a chicken sandwich machine",
+status: "brainstorming")
+
+ProjectOwner.create(user: user, project: project3)
