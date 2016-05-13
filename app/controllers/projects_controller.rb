4d3038856595fb7c1274bc7c8dc190d12a9939ce
Haile Xiao <haile.xiao@gmail.com>
2016-05-13 15:15:22 -0400

    WIP

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,40 @@
+class ProjectsController < ApplicationController
+  def index
+    case params[:status]
+    when 'all'
+      @projects = Project.all
+    when 'completed'
+      @projects = Project.where(status: 'completed')
+    when 'in development'
+      @projects = Project.where(status: 'in development')
+    when 'brainstorming'
+      @projects = Project.where(status: 'brainstorming')
+    else
+      @projects = Project.all
+    end
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+    if @project.save
+      flash[:notice] = "New project added: #{@project.name}"
+      redirect_to project_path(@project)
+    else
+      flash[:error] = @project.errors.full_messages.join(". ") + "."
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
