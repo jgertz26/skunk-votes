9253391ef6bf53de67b9cd4bd88fc0db4e9aa9db
Haile Xiao <haile.xiao@gmail.com>
2016-05-13 13:48:31 -0400

    add project and projectowner model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, :confirmable, 
+  devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
 
   validates :email, presence: true, format: { with: /\S+@paytronix.com\z/, message: "must be of the domain paytronix.com.  You must not be cool enough to work there."}
 
+  has_many :project_owners
+  has_many :projects, through: :project_owners
 end
