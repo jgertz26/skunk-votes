0bd9558c16e55bff61d4530610ed69d9c4614a2d
Jeremy <jgertz26@gmail.com>
2016-05-13 14:43:48 -0400

    forget mailing ever happened

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, :confirmable, 
+  devise :database_authenticatable, :registerable, 
          :recoverable, :rememberable, :trackable, :validatable
 
   validates :email, presence: true, format: { with: /\S+@paytronix.com\z/, message: "must be of the domain paytronix.com.  You must not be cool enough to work there."}
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,8 +48,6 @@ Rails.application.configure do
   # when problems arise.
   config.log_level = :debug
 
-  config.action_mailer.default_url_options = { :host => 'skunk-votes.herokuapp.com' }
-
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
 
